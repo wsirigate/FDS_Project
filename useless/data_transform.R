1762a1bba2d175927a8e45bc7bfcd5b0171a1fa1
wsirigate <workke306@gmail.com>
2019-05-07 22:53:17 +0700

    format files and edit some code

---
+++ useless/data_transform.R
@@ -0,0 +1,15 @@
+setwd("D:/FDS_Project")
+df = read.csv("GDP_per_capita.csv")
+
+dim(df)
+
+us_value <- df[5:62]
+currency <- 31.90 #08/04/62
+
+thb_value <- us_value * currency
+
+df[5:62] = thb_value
+
+df
+
+write.csv(df, "GDP_per_capita_thb.csv")
